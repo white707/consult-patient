a1ec19f58a3fcad5255f1051b006fe0dbc26281c
mizi <16773305+mystic-muzzle@user.noreply.gitee.com>
2026-03-19 09:41:08 +0800

    Initial commit: Vue3+TS project setup

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { reactive } from 'vue'
+const arr = reactive({
+  a: 1,
+  b: 2,
+})
+console.log(arr.a)
+console.log(arr.b)
+</script>
+<template>{{ arr.a }}{{ arr.b }}</template>
+<style scoped></style>
