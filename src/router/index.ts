2b1d1cd8cf62fd5b13d8d89b0c972e96acff5f82
mizi <16773305+mystic-muzzle@user.noreply.gitee.com>
2026-03-23 19:36:41 +0800

    后置守卫标题设置

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,25 +7,31 @@ const router = createRouter({
     {
       path: '/login',
       component: () => import('@/views/login/index.vue'),
+      meta: { title: '登录' },
     },
     {
+      meta: { title: '首页' },
       path: '/',
       redirect: '/home',
       component: () => import('@/views/layout/index.vue'),
       children: [
         {
+          meta: { title: '首页' },
           path: '/home',
           component: () => import('@/views/home/index.vue'),
         },
         {
+          meta: { title: '健康百科' },
           path: '/article',
           component: () => import('@/views/article/index.vue'),
         },
         {
+          meta: { title: '消息通知' },
           path: '/notify',
           component: () => import('@/views/notify/index.vue'),
         },
         {
+          meta: { title: '个人中心' },
           path: '/user',
           component: () => import('@/views/user/index.vue'),
         },
@@ -44,5 +50,9 @@ router.beforeEach((to) => {
     return '/login'
   }
 })
-
+//全局后置守卫
+router.afterEach((to) => {
+  //设置文档标题
+  document.title = `${to.meta.title || ''} - 问诊`
+})
 export default router
