75965ad22f0bb1dffba7986eb1649c94a9e3ef05
mizi <16773305+mystic-muzzle@user.noreply.gitee.com>
2026-03-23 16:31:30 +0800

    布局容器与布局规则

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,6 +7,29 @@ const router = createRouter({
       path: '/login',
       component: () => import('@/views/login/index.vue'),
     },
+    {
+      path: '/',
+      redirect: '/home',
+      component: () => import('@/views/layout/index.vue'),
+      children: [
+        {
+          path: '/home',
+          component: () => import('@/views/home/index.vue'),
+        },
+        {
+          path: '/article',
+          component: () => import('@/views/article/index.vue'),
+        },
+        {
+          path: '/notify',
+          component: () => import('@/views/notify/index.vue'),
+        },
+        {
+          path: '/user',
+          component: () => import('@/views/user/index.vue'),
+        },
+      ],
+    },
   ],
 })
 export default router
---
+++ src/views/user/index.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="user-page"></div>
+</template>
+<style scoped lang="scss"></style>
