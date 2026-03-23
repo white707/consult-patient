fe496d1868f6563e541014c0e842d9d9d3d25a47
mizi <16773305+mystic-muzzle@user.noreply.gitee.com>
2026-03-23 08:48:05 +0800

    登陆布局

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts"></script>
 <template>
-  <div class="app">
-    <!-- 这里是 路由出口 -->
-    <router-view />
-  </div>
+  <!-- 这里是 路由出口 -->
+  <router-view />
 </template>
 <style scoped lang="scss"></style>
