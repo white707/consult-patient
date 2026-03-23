18618954f32bab3fae2e08897266b22d8753e74d
mizi <16773305+mystic-muzzle@user.noreply.gitee.com>
2026-03-23 15:18:08 +0800

    打包svg地图

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import router from './router'
 import 'vant/lib/index.css'
 import './styles/main.scss'
 
+//注册svg图标
+import 'virtual:svg-icons-register'
+
 const app = createApp(App)
 
 app.use(pinia)
--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,22 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+//配置svg图标插件
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import path from 'path'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    createSvgIconsPlugin({
+      iconDirs: [path.resolve(process.cwd(), 'src/icons')],
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
