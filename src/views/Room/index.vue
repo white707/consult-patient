2cd378bce56b8f893438603ac6510dd71f91f22a
mizi <16773305+mystic-muzzle@user.noreply.gitee.com>
2026-04-08 20:03:29 +0800

    feat:问诊室页面搭建-组件拆分

---
+++ src/views/Room/index.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import RoomMessage from './components/RoomMessage.vue'
+import RoomAction from './components/RoomAction.vue'
+import CpNavBar from '@/components/cpNavBar.vue'
+import RoomStatus from './components/RoomStatus.vue'
+</script>
+<template>
+  <div class="room-page">
+    <cp-nav-bar title="问诊室"></cp-nav-bar>
+    <!-- 咨询状态栏 -->
+    <RoomStatus />
+    <!-- 咨询操作栏 -->
+    <RoomAction />
+    <!-- 咨询消息框 -->
+    <RoomMessage />
+  </div>
+</template>
+<style lang="scss" scoped>
+.room-page {
+  padding-top: 90px;
+  padding-bottom: 60px;
+  min-height: 100vh;
+  box-sizing: border-box;
+  background-color: var(--cp-bg);
+  .van-pull-refresh {
+    width: 100%;
+    min-height: calc(100vh - 150px);
+  }
+}
+</style>
